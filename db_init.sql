a4e73bf92108997ec1985e97ccf3edf9e06a9668
Alistair Davies <ali17sam@gmail.com>
2018-10-10 22:02:06 +0100

    script to generate tea pages, file rearrange

---
+++ db_init.sql
@@ -0,0 +1,43 @@
+DROP DATABASE IF EXISTS felstedreq;
+CREATE DATABASE felstedreq;
+USE felstedreq;
+
+CREATE TABLE teacher (
+  name VARCHAR(20),
+  teacher_id CHAR(3),
+  PRIMARY KEY(teacher_id)
+);
+
+CREATE TABLE technician (
+  name VARCHAR(20),
+  subject ENUM('CHEM', 'PHYS', 'BIOL'),
+  technician_id CHAR(3),
+  PRIMARY KEY(technician_id)
+);
+
+CREATE TABLE lesson (
+  name VARCHAR(20),
+  room VARCHAR(20),
+  day ENUM('MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'),
+  period TINYINT,
+  week ENUM('A', 'B'),
+  subject ENUM('CHEM', 'PHYS', 'BIOL'),
+  lesson_id INT NOT NULL AUTO_INCREMENT,
+  teacher_id CHAR(3),
+  PRIMARY KEY(lesson_id),
+  FOREIGN KEY (teacher_id) REFERENCES teacher(teacher_id)
+);
+
+CREATE TABLE requisition (
+  requisition_id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(40),
+  description VARCHAR(1000),
+  risk_assessment ENUM('YES', 'NO'),
+  risk_actions VARCHAR(400), 
+  lesson_id INT,
+  done_bool tinyint(1),
+  PRIMARY KEY(requisition_id),
+  FOREIGN KEY (lesson_id) REFERENCES lesson(lesson_id)
+); 
+
+INSERT INTO technician VALUES ('Karen','CHEM', 'kjd');
---
+++ setup.py
@@ -0,0 +1,129 @@
+import csv
+import xlrd
+import os
+
+def csv_from_excel():
+    wb = xlrd.open_workbook('MasterStaffTT Sc 030918.xlsx')
+    sh = wb.sheet_by_name('Staff Timetable')
+    timetable = open('timetable.csv', 'w')
+    wr = csv.writer(timetable, quoting=csv.QUOTE_ALL)
+
+    for rownum in range(sh.nrows):
+        wr.writerow(sh.row_values(rownum))
+
+    timetable.close()
+
+def listTeachers(data):
+    teachers=[]
+    numrows = len(data)
+    row = 4
+    while (row < numrows):
+        teachers.append(data[row][0].strip())
+        row+=2
+    return teachers
+
+def getTeacherLesson(teacher, data):
+    column=0
+    lessons = []
+    days = ['MON||A||', 'TUE||A||', 'WED||A||', 'THU||A||', 'FRI||A||', 'SAT||A||',
+            'MON||B||', 'TUE||B||', 'WED||B||', 'THU||B||', 'FRI||B||', 'SAT||B||']
+    day = days[0]
+    c=0
+    while column < len(data)-1:
+        column+=1
+        if (column%7==0):
+            c+=1
+            day=days[c]
+            #print(day)
+            continue
+        elif (data[column] == "\r\n\r\n" or data[column] == ''):
+            continue
+        else:
+            period = str(column%7)+"||"
+            less = period + day + data[column].replace('\r\n', '||')
+            lessons.append(less)
+        
+    return lessons
+
+def insertTeacherSQL(teacher):
+    statement = "INSERT INTO teacher VALUES ('"+teacher+"','"+teacher+"');\n"
+    return statement
+
+def insertTeachersLessons(teacher, lessons):
+    statements=[]
+    for i in lessons:
+        less = processLesson(i, teacher)
+        if less != 0:
+            statements.append(less)
+    return statements
+
+def processLesson(lesson, teacher):
+    if "||||" in lesson:
+        print("Disregarding ["+lesson.replace('||', ' ') +"] as suspected invalid lesson")
+        return 0
+    else:
+      try:
+          d = lesson.split("||")
+      except:
+          print("Not a valid lesson: "+lesson.replace('||', ' '))
+          return 0
+      period = d[0]
+      day = d[1]
+      week = d[2]
+      subject = d[3]
+      if subject == 'BI':
+          subject = 'BIOL'
+      elif subject == 'CH':
+          subject = 'CHEM'
+      elif subject == 'PH':
+          subject = 'PHYS'
+      else:
+           print("Not a valid subject: "+lesson.replace('||', ' '))
+           return 0
+      group = d[4]
+      room = d[5]
+      return "INSERT INTO lesson VALUES ('"+group+"','"+room+"','"+day+"',"+period+",'"+week+"','"+subject+"',NULL,'"+teacher+"');\n"
+
+def insertTeacherHTML(teacher):
+    return "<?php\n\trequire(\"functions.php\");\n\tif(!isset($_GET['teach'])\
+||!strcmp($_GET['teach'], '"+teacher+"')){\n\t\t$teach=\""+teacher+"\";\n\
+\t\t$enableEdit=1;\n\t}\n\telse{\n\t\t$teach=$_GET['teach'];\n\t\t$enableEdit=0;\n\t}\n\trequire\
+(\"defaultlayout_teach.php\");\n?>"
+    
+csv_from_excel()
+
+
+with open('timetable.csv','r', newline='') as f:
+    reader = csv.reader(f)
+    data=list(reader)
+    teachers = listTeachers(data)
+    row = 4
+    teacherLessons = {}
+    for teacher in teachers:
+        teacherLessons[teacher] = getTeacherLesson(teacher, data[row])
+        row+=2
+    f.close()
+
+chemTeachers = ["AJP", "HJM", "LEB", "SMG"]
+here = os.path.dirname(os.path.realpath(__file__))
+directory = "main"
+
+for teacher in chemTeachers:
+    x = "teacher_"+teacher.lower()+".php"
+    filepath = os.path.join(here, directory, x)
+    try:
+        f = open(filepath, 'w')
+        f.write(insertTeacherHTML(teacher))
+        f.close()
+    except IOError as e:
+        print ("problem with path: "+filepath)
+        print(e)
+
+f = open("db_insert.sql", "w")
+
+for teacher in teacherLessons:
+    f.write(insertTeacherSQL(teacher))
+    lessons = insertTeachersLessons(teacher, teacherLessons[teacher])
+    for lessonquery in lessons:
+        f.write(lessonquery)
+                            
