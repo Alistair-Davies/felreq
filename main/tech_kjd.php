acb1585301757fb806cc43f0a4e11da6bbfaf3e5
Alistair Davies <ali17sam@gmail.com>
2018-10-23 11:33:02 +0100

    return link

--- main/tech_kjd.php
+++ main/tech_kjd.php
@@ -1,7 +1,7 @@
 <?php
   require("functions.php");
   check_user('tech');
-  db_connect();
+  $link = db_connect();
   require("functions_tech.php");
   $subject = "CHEM";
   require("defaultlayout_tech.php");
